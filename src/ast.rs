4ad0477545fd24bd28828884de2b026fc416722d
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-07-27 22:07:43 -0400

    Make all expressions formattable, except expressions involving types

--- src/ast.rs
+++ src/ast.rs
@@ -13,6 +13,22 @@ struct Include {
     path: String,
 }
 
+impl std::fmt::Display for Include {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let (quote1, quote2) = match &self.method {
+            IncludeMethod::Quote => ("\"", "\""),
+            IncludeMethod::Bracket => ("<", ">"),
+        };
+        write!(
+            f,
+            "#include {quote1}{path}{quote2}",
+            path = self.path,
+            quote1 = quote1,
+            quote2 = quote2
+        )
+    }
+}
+
 enum Literal {
     Signed(i128),
     SignedLong(i128),
@@ -21,16 +37,56 @@ enum Literal {
     UnsignedLong(u128),
     UnsignedLongLong(u128),
     Character(char),
+    WideCharacter(char),
     Float(FloatConstant),
     Double(FloatConstant),
     LongDouble(FloatConstant),
     String(String),
+    WideString(Vec<char>),
+}
+
+impl std::fmt::Display for Literal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Literal::Signed(val) => write!(f, "{}", val),
+            Literal::SignedLong(val) => write!(f, "{}L", val),
+            Literal::SignedLongLong(val) => write!(f, "{}LL", val),
+            Literal::Unsigned(val) => write!(f, "{}U", val),
+            Literal::UnsignedLong(val) => write!(f, "{}UL", val),
+            Literal::UnsignedLongLong(val) => write!(f, "{}ULL", val),
+            Literal::Character(val) => write!(f, "'{}'", val.escape_default()),
+            Literal::WideCharacter(val) => write!(f, "L'{}'", val.escape_default()),
+            Literal::Float(val) => write!(f, "{}f", val),
+            Literal::Double(val) => write!(f, "{}", val),
+            Literal::LongDouble(val) => write!(f, "{}L", val),
+            Literal::String(val) => write!(f, "\"{}\"", val.as_str().escape_default()),
+            Literal::WideString(val) => write!(
+                f,
+                "L\"{}\"",
+                val.iter()
+                    .map(|x| x.escape_default().to_string())
+                    .collect::<String>()
+            ),
+        }
+    }
 }
 
 struct FloatConstant {
     integer: i128,
     fraction: u128,
     exponent: i128,
+}
+
+impl std::fmt::Display for FloatConstant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{integer}.{fraction}e{exponent}",
+            integer = self.integer,
+            fraction = self.fraction,
+            exponent = self.exponent
+        )
+    }
 }
 
 enum Expression {
@@ -40,12 +96,57 @@ enum Expression {
     Unary(Box<UnaryExpression>),
     Binary(Box<BinaryExpression>),
     Ternary(Box<TernaryExpression>),
-    Assignment(Box<AssignmentExpression>),
+}
+
+impl std::fmt::Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Identifier(val) => write!(f, "{}", val),
+            Expression::Literal(val) => write!(f, "{}", val),
+            Expression::Sizeof(val) => write!(f, "sizeof({})", val),
+            Expression::Unary(val) => write!(f, "{}", val),
+            Expression::Binary(val) => write!(f, "{}", val),
+            Expression::Ternary(val) => write!(f, "{}", val),
+        }
+    }
 }
 
 struct UnaryExpression {
     expression: Expression,
     operation: UnaryOperation,
+}
+
+impl std::fmt::Display for UnaryExpression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.operation {
+            UnaryOperation::PrefixIncrement => write!(f, "++({})", self.expression),
+            UnaryOperation::PrefixDecrement => write!(f, "--({})", self.expression),
+            UnaryOperation::PostfixIncrement => write!(f, "({})++", self.expression),
+            UnaryOperation::PostfixDecrement => write!(f, "({})--", self.expression),
+            UnaryOperation::Address => write!(f, "&({})", self.expression),
+            UnaryOperation::Dereference => write!(f, "*({})", self.expression),
+            UnaryOperation::Plus => write!(f, "+({})", self.expression),
+            UnaryOperation::Minus => write!(f, "-({})", self.expression),
+            UnaryOperation::BitwiseNegate => write!(f, "~({})", self.expression),
+            UnaryOperation::LogicalNegate => write!(f, "!({})", self.expression),
+            UnaryOperation::Sizeof => write!(f, "sizeof({})", self.expression),
+            UnaryOperation::Cast(casted) => write!(
+                f,
+                "({casted})({exp})",
+                casted = casted,
+                exp = self.expression
+            ),
+            UnaryOperation::ArrayAccess(index) => {
+                write!(f, "({exp})[{index}]", exp = self.expression, index = index)
+            }
+            UnaryOperation::StructAccess(field) => {
+                write!(f, "({exp}).{field}", exp = self.expression, field = field)
+            }
+            UnaryOperation::StructDereference(field) => {
+                write!(f, "({exp})->{field}", exp = self.expression, field = field)
+            }
+        }
+    }
 }
 
 enum UnaryOperation {
@@ -57,10 +158,10 @@ enum UnaryOperation {
     Dereference,
     Plus,
     Minus,
-    Negate,
+    BitwiseNegate,
     LogicalNegate,
     Sizeof,
-    Cast(UnqualifiedType),
+    Cast(QualifiedType),
     ArrayAccess(Expression),
     StructAccess(String),
     StructDereference(String),
@@ -70,6 +171,50 @@ struct BinaryExpression {
     left: Expression,
     right: Expression,
     operation: BinaryOperation,
+}
+
+impl std::fmt::Display for BinaryExpression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let symbol = match self.operation {
+            BinaryOperation::Add => "+",
+            BinaryOperation::Subtract => "-",
+            BinaryOperation::Multiply => "*",
+            BinaryOperation::Divide => "/",
+            BinaryOperation::Modulo => "%",
+            BinaryOperation::LeftShift => "<<",
+            BinaryOperation::RightShift => ">>",
+            BinaryOperation::LessThan => "<",
+            BinaryOperation::GreaterThan => ">",
+            BinaryOperation::LessThanEqual => "<=",
+            BinaryOperation::GreaterThanEqual => ">=",
+            BinaryOperation::Equal => "==",
+            BinaryOperation::NotEqual => "!=",
+            BinaryOperation::BitwiseAnd => "&",
+            BinaryOperation::BitwiseOr => "|",
+            BinaryOperation::BitwiseXor => "^",
+            BinaryOperation::LogicalAnd => "&&",
+            BinaryOperation::LogicalOr => "||",
+            BinaryOperation::Comma => ",",
+            BinaryOperation::Assign => "=",
+            BinaryOperation::AddAssign => "+=",
+            BinaryOperation::SubtractAssign => "-=",
+            BinaryOperation::MultiplyAssign => "*=",
+            BinaryOperation::DivideAssign => "/=",
+            BinaryOperation::ModuloAssign => "%=",
+            BinaryOperation::LeftShiftAssign => "<<=",
+            BinaryOperation::RightShiftAssign => ">>=",
+            BinaryOperation::BitwiseAndAssign => "&=",
+            BinaryOperation::BitwiseOrAssign => "|=",
+            BinaryOperation::BitwiseXorAssign => "^=",
+        };
+        write!(
+            f,
+            "{left} {symbol} {right}",
+            left = self.left,
+            symbol = symbol,
+            right = self.right
+        )
+    }
 }
 
 enum BinaryOperation {
@@ -92,6 +237,17 @@ enum BinaryOperation {
     LogicalAnd,
     LogicalOr,
     Comma,
+    Assign,
+    AddAssign,
+    SubtractAssign,
+    MultiplyAssign,
+    DivideAssign,
+    ModuloAssign,
+    LeftShiftAssign,
+    RightShiftAssign,
+    BitwiseAndAssign,
+    BitwiseOrAssign,
+    BitwiseXorAssign,
 }
 
 struct TernaryExpression {
@@ -100,24 +256,16 @@ struct TernaryExpression {
     if_false: Expression,
 }
 
-struct AssignmentExpression {
-    left: Expression,
-    right: Expression,
-    operation: AssignmentOperation,
-}
-
-enum AssignmentOperation {
-    Assign,
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Modulo,
-    LeftShift,
-    RightShift,
-    BitwiseAnd,
-    BitwiseOr,
-    BitwiseXor,
+impl std::fmt::Display for TernaryExpression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "({condition}) ? ({if_true}) : ({if_false})",
+            condition = self.condition,
+            if_true = self.if_true,
+            if_false = self.if_false
+        )
+    }
 }
 
 enum Storage {
@@ -128,9 +276,33 @@ enum Storage {
     Typedef,
 }
 
-struct Qualifier {
+impl std::fmt::Display for Storage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Storage::Auto => "auto",
+                Storage::Static => "static",
+                Storage::Register => "register",
+                Storage::Extern => "extern",
+                Storage::Typedef => "typedef",
+            }
+        )
+    }
+}
+
+struct CVQualifier {
     constant: bool,
     volatile: bool,
+}
+
+impl std::fmt::Display for CVQualifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = if self.constant { "const" } else { "" };
+        let v = if self.volatile { "volatile" } else { "" };
+        write!(f, "{c} {v}", c = c, v = v)
+    }
 }
 
 enum UnqualifiedType {
@@ -147,13 +319,19 @@ struct ArrayType {
 }
 
 struct QualifiedType {
-    qualifier: Qualifier,
+    qualifier: CVQualifier,
     unqualified_type: UnqualifiedType,
+}
+
+impl std::fmt::Display for QualifiedType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        unimplemented!()
+    }
 }
 
 struct Declaration {
     storage: Option<Storage>,
-    qualifier: Qualifier,
+    qualifier: CVQualifier,
     qualified_type: QualifiedType,
 }
 
